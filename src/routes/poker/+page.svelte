17c7e33555c2a9c13862887e61c17b44b5d7f2ea
Arthur <rthurmedeiros@gmail.com>
2023-10-24 00:10:50 -0300

    Added votes and comments to interface

--- src/routes/poker/+page.svelte
+++ src/routes/poker/+page.svelte
@@ -1,4 +1,12 @@
-<section>
+<script>
+	const votes = ['1', '2', '3', '4'];
+	const comments = [
+		'Acho que seria importante considerar os testes para a estimativa de esforços.',
+		'Precisa definir bem quais tipos de ingredientes poderão estar na checklist.'
+	];
+</script>
+
+<section style="padding-bottom: 10em;">
 	<div class="flex flex-row justify-center">
 		<div id="poker-main" class="flex flex-col gap-6 max-w-lg">
 			<h3>Sessão de planning poker #1</h3>
@@ -10,6 +18,39 @@
 				possimus facere nostrum voluptates eligendi quo? Laborum vero corporis, quam est, similique
 				nulla officiis mollitia placeat praesentium ipsum iste?
 			</p>
+			<div id="poker-voting" class="flex gap-2">
+				{#each votes as vote}
+					<div class="btn btn-circle btn-info">
+						{vote}
+					</div>
+				{/each}
+				<div class="flex-grow" />
+				<button class="btn btn-circle btn-accent"> 👁️ </button>
+				<button class="btn btn-circle btn-accent"> ✅ </button>
+			</div>
+			<div id="poker-comment-area">
+                <input type="text" placeholder="Adicionar comentário" class="input input-bordered w-full" />
+			</div>
+            {#each comments as comment}
+                <div class="card border-2">
+                    <div class="card-body flex flex-row gap-8">
+                        <button class="btn btn-circle btn-info"></button>
+                        <div>
+                            <p>
+                                {comment}
+                            </p>
+                            <div class="text-right">
+                                <p class="text-gray-500">
+                                    01/10/2023, 18:27
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            {/each}
+            <p class="text-center">
+                Estimativa definida como <strong>5</strong> story points
+            </p>
 		</div>
 	</div>
 </section>
