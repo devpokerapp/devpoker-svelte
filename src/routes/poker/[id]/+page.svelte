eb11d35893722c0d856a8de9e16d2719c5fd8005
Arthur <rthurmedeiros@gmail.com>
2023-11-05 18:48:06 -0300

    Moved user story menu to component

--- src/routes/poker/[id]/+page.svelte
+++ src/routes/poker/[id]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { getContext, onMount } from 'svelte';
-	import Deck from './Deck.svelte';
 	import type { PageData } from './$types';
+	import Deck from './Deck.svelte';
+	import UserStoryMenu from './UserStoryMenu.svelte';
 
 	export let data: PageData;
 
@@ -90,32 +91,7 @@
 				<div class="p-4">
 					<div class="btn btn-circle btn-neutral"> &gt; </div>
 				</div>
-				<div id="story-menu" class="card border border-gray-300 shadow w-72 bg-white">
-					<div class="card-body">
-						<h3 class="card-title text-center text-2xl"> User Stories </h3>
-						<div class="py-6">
-							<div class="flex flex-row">
-								<button class="text-left"> Implementar sistema de comentários </button>
-								<div class="btn btn-sm btn-circle btn-info">5</div>
-							</div>
-							<div class="divider my-3"></div>
-							<div class="flex flex-row">
-								<button class="text-left font-bold"> Adicionar checklist de ingredientes </button>
-								<div class="btn btn-sm btn-circle btn-ghost">...</div>
-							</div>
-							<div class="divider my-3"></div>
-							<div class="flex flex-row">
-								<button class="text-left"> Corrigir falha de segurança </button>
-								<div class="btn btn-sm btn-circle btn-ghost">...</div>
-							</div>
-						</div>
-						<div class="card-actions">
-							<div class="btn btn-primary w-full">
-								+ Nova US
-							</div>
-						</div>
-					</div>
-				</div>
+				<UserStoryMenu />
 			</div>
 		</div>
 		<div class="fixed bottom-0 pb-8">
---
+++ src/routes/poker/[id]/UserStoryMenu.svelte
@@ -0,0 +1,26 @@
+<div id="story-menu" class="card border border-gray-300 shadow w-72 bg-white">
+    <div class="card-body">
+        <h3 class="card-title text-center text-2xl"> User Stories </h3>
+        <div class="py-6">
+            <div class="flex flex-row">
+                <button class="text-left"> Implementar sistema de comentários </button>
+                <div class="btn btn-sm btn-circle btn-info">5</div>
+            </div>
+            <div class="divider my-3"></div>
+            <div class="flex flex-row">
+                <button class="text-left font-bold"> Adicionar checklist de ingredientes </button>
+                <div class="btn btn-sm btn-circle btn-ghost">...</div>
+            </div>
+            <div class="divider my-3"></div>
+            <div class="flex flex-row">
+                <button class="text-left"> Corrigir falha de segurança </button>
+                <div class="btn btn-sm btn-circle btn-ghost">...</div>
+            </div>
+        </div>
+        <div class="card-actions">
+            <div class="btn btn-primary w-full">
+                + Nova US
+            </div>
+        </div>
+    </div>
+</div>
