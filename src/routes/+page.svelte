3d38e714af6c3edac33bacb7d64b2be9d087c164
Arthur <rthurmedeiros@gmail.com>
2023-11-12 00:47:41 -0300

    Added poker creation modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,39 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { getContext } from 'svelte';
+	import { closeModal, openModal } from '../util/modal';
 
+	let name: string = '';
 	let loading = false;
 
 	const websocket = getContext<IWebSocketContext>('websocket');
 
-	const start = () => {
-		websocket
-			.sendAndWait({
+	const handleStartCreatePoker = () => {
+		openModal('modal-poker-create')
+	};
+
+	const handleCreatePoker = async (event: SubmitEvent) => {
+		event.preventDefault();
+		let loading = false;
+
+		try {
+			const response = await websocket.sendAndWait({
 				service: 'poker_service',
 				method: 'create',
 				data: {
 					payload: {
-						creator: ''
+						creator: name
+						// TODO: get a better source for the creator code
 					}
 				}
-			})
-			.then((response) => {
-				const poker = response.result as Poker;
-				goto(`/poker/${poker.id}`);
-			})
-			.catch((response) => {
-				console.error('REJECTED: ', response);
 			});
-		loading = true;
+			const poker = response.result as Poker;
+			goto(`/poker/${poker.id}`);
+		} catch (error) {
+			console.error('REJECTED: ', error);
+		} finally {
+			loading = true;
+		}
 	};
 </script>
 
@@ -43,7 +52,7 @@
 					soluta. Unde itaque reprehenderit voluptatibus sed tempore? Inventore vel excepturi ab
 					blanditiis laborum nulla itaque enim saepe deleniti.
 				</p>
-				<button class="btn btn-primary" on:click={start} disabled={loading}>
+				<button class="btn btn-primary" on:click={handleStartCreatePoker} disabled={loading}>
 					{#if loading}
 						<span class="loading loading-spinner loading-xs" />
 					{/if}
@@ -52,4 +61,27 @@
 			</div>
 		</div>
 	</div>
+	<!-- Create Poker -->
+	<dialog id="modal-poker-create" class="modal modal-bottom sm:modal-middle">
+		<form method="dialog" class="modal-box flex flex-col gap-4" on:submit={handleCreatePoker}>
+			<h3 class="font-bold text-xl pb-2">Iniciar sessão de Planning Poker</h3>
+			<input
+				type="text"
+				placeholder="Seu nome"
+				class="input input-bordered w-full"
+				bind:value={name}
+			/>
+			<div class="modal-action">
+				<div class="flex flex-row gap-4">
+					<button type="reset" class="btn" on:click={() => closeModal('modal-poker-create')}>
+						Cancelar
+					</button>
+					<button type="submit" class="btn btn-primary" disabled={loading}> Confirmar </button>
+				</div>
+			</div>
+		</form>
+		<form method="dialog" class="modal-backdrop">
+			<button>close</button>
+		</form>
+	</dialog>
 </section>
