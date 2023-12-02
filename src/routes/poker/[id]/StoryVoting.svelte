4924a52f3eb39fb1832d3a127f118d334206713e
Arthur <rthurmedeiros@gmail.com>
2023-12-02 11:23:28 -0300

    Implemented highlight to most voted values

--- src/routes/poker/[id]/StoryVoting.svelte
+++ src/routes/poker/[id]/StoryVoting.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
 	import { get, type Writable } from 'svelte/store';
+	import { getMostUsed } from '../../../util/operations';
+
+	const VALID_VALUES = ['0', '1', '2', '3', '5', '8', '13']; // '?', '☕'];
 
 	const websocket = getContext<IWebSocketContext>('websocket');
 	const storyContext = getContext<IStoryContext>('story');
@@ -19,11 +22,14 @@
 	const { entities: votes }: { entities: Writable<Vote[]> } = voteContext;
 	const { current: currentPolling }: { current: Writable<Polling | undefined> } = pollingContext;
 
-	const validValues = ['0', '1', '2', '3', '5', '8', '13']; // '?', '☕'];
+	let mostVoted: string[] = [];
 
-	websocket.listen('poker_selected_story', (message) => {
-		entities.set([]);
-	});
+	const computedMostVoted = () => {
+		const values = get(votes).map((e) => e.value);
+		mostVoted = getMostUsed(values);
+	};
+
+	votes.subscribe(computedMostVoted);
 
 	const handleReveal = () => {
 		const polling = get(currentPolling);
@@ -87,12 +93,32 @@
 				✅
 			</button>
 			{#if !$currentPolling?.completed}
-				<ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-36">
+				<ul class="dropdown-content z-[1] menu p-2 shadow bg-accent rounded-box w-40">
 					<h2 class="menu-title">Estimativa</h2>
-					{#each validValues as value}
+					{#each VALID_VALUES as value}
 						<li>
-							<!-- TODO: highlight most voted -->
-							<button on:click={() => handleComplete(value)}>{value} pontos</button>
+							<button
+								on:click={() => handleComplete(value)}
+								class="flex flex-row tooltip tooltip-bottom"
+								data-tip={mostVoted.includes(value) ? 'Mais votado!' : undefined}
+							>
+								<span>{value} pontos</span>
+								<span class="flex-grow" />
+								{#if mostVoted.includes(value)}
+									<span class="text-yellow-500">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 20 20"
+											fill="currentColor"
+											class="w-5 h-5"
+										>
+											<path
+												d="M15.98 1.804a1 1 0 00-1.96 0l-.24 1.192a1 1 0 01-.784.785l-1.192.238a1 1 0 000 1.962l1.192.238a1 1 0 01.785.785l.238 1.192a1 1 0 001.962 0l.238-1.192a1 1 0 01.785-.785l1.192-.238a1 1 0 000-1.962l-1.192-.238a1 1 0 01-.785-.785l-.238-1.192zM6.949 5.684a1 1 0 00-1.898 0l-.683 2.051a1 1 0 01-.633.633l-2.051.683a1 1 0 000 1.898l2.051.684a1 1 0 01.633.632l.683 2.051a1 1 0 001.898 0l.683-2.051a1 1 0 01.633-.633l2.051-.683a1 1 0 000-1.898l-2.051-.683a1 1 0 01-.633-.633L6.95 5.684zM13.949 13.684a1 1 0 00-1.898 0l-.184.551a1 1 0 01-.632.633l-.551.183a1 1 0 000 1.898l.551.183a1 1 0 01.633.633l.183.551a1 1 0 001.898 0l.184-.551a1 1 0 01.632-.633l.551-.183a1 1 0 000-1.898l-.551-.184a1 1 0 01-.633-.632l-.183-.551z"
+											/>
+										</svg>
+									</span>
+								{/if}
+							</button>
 						</li>
 					{/each}
 				</ul>
