6dacee46ea085f41ad75a82abff9674134412592
Arthur <rthurmedeiros@gmail.com>
2023-12-06 22:43:59 -0300

    Changed story dropdown icon

--- src/routes/poker/[id]/StoryMenu.svelte
+++ src/routes/poker/[id]/StoryMenu.svelte
@@ -144,7 +144,20 @@
 					<!-- buttons -->
 					<!-- <button class="btn btn-sm btn-circle btn-info">5</button> -->
 					<div class="dropdown dropdown-end">
-						<button tabindex="0" class="btn btn-sm btn-circle btn-ghost"> ... </button>
+						<button tabindex="0" class="btn btn-sm btn-circle btn-ghost">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</button>
 						<ul
 							tabindex="-1"
 							class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
