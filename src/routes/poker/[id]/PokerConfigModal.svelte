22366fffc07ba459ff9e8bf01ab56a1a158152c1
Arthur <rthurmedeiros@gmail.com>
2023-12-13 20:32:50 -0300

    Implemented text based custom vote pattern

--- src/routes/poker/[id]/PokerConfigModal.svelte
+++ src/routes/poker/[id]/PokerConfigModal.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
-	import { get, writable, type Writable } from 'svelte/store';
+	import type { KeyboardEventHandler } from 'svelte/elements';
+	import { get, type Writable } from 'svelte/store';
 	import VoteLabel from '../../../components/VoteLabel.svelte';
 	import { closeModal } from '../../../util/modal';
-	import VotePatternManager from './VotePatternManager.svelte';
 
+	type VotePatternType = 'fibo' | 'tshirt' | 'custom';
 	interface VotePattern {
-		type: string;
+		type: VotePatternType;
 		name: string;
 		value?: string;
 	}
@@ -34,13 +35,25 @@
 	const pokerContext = getContext<IPokerContext>('poker');
 	const { current: currentPoker }: { current: Writable<Poker | undefined> } = pokerContext;
 
+	const getOptionsFromPattern = (textPattern: string) => {
+		return textPattern.split(',');
+	};
+
 	let votePattern = PATTERN_FIBONACCI;
-	let voteType = 'fibo';
-	const customVoteOptions = writable(PATTERN_FIBONACCI.split(','));
+	let voteType: VotePatternType = 'fibo';
+	let voteOptions = getOptionsFromPattern(votePattern);
 
 	currentPoker.subscribe((value) => {
-		votePattern = value?.votePattern || '';
+		votePattern = value?.votePattern || PATTERN_FIBONACCI;
 	});
+
+	const changePattern = (pattern: VotePattern) => {
+		voteType = pattern.type;
+		if (pattern.value !== undefined) {
+			votePattern = pattern.value;
+			voteOptions = getOptionsFromPattern(votePattern);
+		}
+	};
 
 	const handlePokerUpdate = async (event: SubmitEvent) => {
 		event.preventDefault();
@@ -48,10 +61,6 @@
 		const current = get(currentPoker);
 		if (current === undefined) {
 			return;
-		}
-
-		if (voteType === 'custom') {
-			votePattern = get(customVoteOptions).join(',');
 		}
 
 		try {
@@ -66,12 +75,8 @@
 		}
 	};
 
-	const switchVotePattern = (pattern: VotePattern) => {
-		voteType = pattern.type;
-		if (pattern.value !== undefined) {
-			votePattern = pattern.value;
-		}
-		// TODO: show editing interface
+	const handleCustomPatternKeydown: KeyboardEventHandler<HTMLInputElement> = async (event) => {
+		voteOptions = getOptionsFromPattern(votePattern);
 	};
 </script>
 
@@ -99,22 +104,29 @@
 					<button
 						type="button"
 						class="card text-start join-item border"
-						on:click={() => switchVotePattern(pattern)}
+						on:click={() => changePattern(pattern)}
 					>
 						<div class="card-body flex flex-row">
 							<div class="flex-grow">
 								<div>
 									{pattern.name}
 								</div>
-								{#if pattern.value}
-									<div class="flex flex-row gap-1 pt-2">
-										{#each pattern.value.split(',') as value}
+								<div class="flex flex-row gap-1 pt-2">
+									{#if pattern.value}
+										{#each getOptionsFromPattern(pattern.value) as value}
 											<div class="badge badge-secondary">
 												<VoteLabel {value} />
 											</div>
 										{/each}
-									</div>
-								{/if}
+									{/if}
+									{#if pattern.type == 'custom' && voteType == 'custom'}
+										{#each voteOptions as value}
+											<div class="badge badge-secondary">
+												<VoteLabel {value} />
+											</div>
+										{/each}
+									{/if}
+								</div>
 							</div>
 							<input
 								id="votetype-{pattern.type}"
@@ -125,20 +137,30 @@
 								bind:group={voteType}
 							/>
 						</div>
+						{#if pattern.type == 'custom' && voteType == 'custom'}
+							<div class="card-body pt-0">
+								<div class="form-control w-full">
+									<div class="label">
+										<span class="label-text">Padrão de votos válidos</span>
+									</div>
+									<input
+										type="text"
+										class="input input-bordered w-full"
+										placeholder="Exemplo: 0,1,2,3,5,8,?"
+										bind:value={votePattern}
+										on:keyup={handleCustomPatternKeydown}
+										on:focus={() => changePattern(pattern)}
+									/>
+									<div class="label">
+										<span class="label-text"> Valores separados por vírgula (",") </span>
+									</div>
+								</div>
+							</div>
+						{/if}
 					</button>
 				{/each}
 			</div>
 		</div>
-		{#if voteType === 'custom'}
-			<div class="form-control w-full">
-				<div class="label">
-					<span class="label-text">Opções de voto</span>
-				</div>
-				<div class="card border">
-					<VotePatternManager options={$customVoteOptions} />
-				</div>
-			</div>
-		{/if}
 		<div class="modal-action">
 			<div class="flex flex-row gap-4">
 				<button type="button" class="btn" on:click={() => closeModal('modal-poker-config')}>
