6abb24a9c2cbea8a4316cd0d05cbc32c9ffed621
Arthur <rthurmedeiros@gmail.com>
2023-12-20 22:43:27 -0300

    Changed story menu to a modal

--- src/routes/poker/[id]/StoryMenu.svelte
+++ src/routes/poker/[id]/StoryMenu.svelte
@@ -159,17 +159,10 @@
 					<button
 						tabindex="0"
 						class="btn btn-sm btn-circle btn-ghost"
-						on:focus={(ev) => {
+						on:click={(ev) => {
 							focusMenuHeight = ev.currentTarget.offsetTop - scrollingContainer.scrollTop;
 							focusing = story;
-							showFocused = true;
-						}}
-						on:blur={() => {
-							if (focusing !== undefined && focusing.id === story.id) {
-								setTimeout(() => {
-									showFocused = false;
-								}, SMALL_DELAY_TO_ALLOW_CLICKS);
-							}
+							openModal('modal-story-options');
 						}}
 					>
 						<svg
@@ -283,66 +276,69 @@
 		</form>
 	</dialog>
 	<!-- Story context menu -->
-	<ul
-		tabindex="-1"
-		class="z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 absolute right-6"
-		class:hidden={!showFocused}
-		style="top: {focusMenuHeight + 32}px;"
-	>
-		<li>
-			<button on:click={() => focusing !== undefined && pokerContext.selectStory(focusing.id)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="w-5 h-5"
+	<dialog id="modal-story-options" class="modal">
+		<ul
+			class="modal-box p-2 menu rounded-box w-52 absolute left-20"
+			style="top: {focusMenuHeight + 32}px;"
+		>
+			<li>
+				<button on:click={() => focusing !== undefined && pokerContext.selectStory(focusing.id)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="w-5 h-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M10 1a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 1zM5.05 3.05a.75.75 0 011.06 0l1.062 1.06A.75.75 0 116.11 5.173L5.05 4.11a.75.75 0 010-1.06zm9.9 0a.75.75 0 010 1.06l-1.06 1.062a.75.75 0 01-1.062-1.061l1.061-1.06a.75.75 0 011.06 0zM3 8a.75.75 0 01.75-.75h1.5a.75.75 0 010 1.5h-1.5A.75.75 0 013 8zm11 0a.75.75 0 01.75-.75h1.5a.75.75 0 010 1.5h-1.5A.75.75 0 0114 8zm-6.828 2.828a.75.75 0 010 1.061L6.11 12.95a.75.75 0 01-1.06-1.06l1.06-1.06a.75.75 0 011.06 0zm3.594-3.317a.75.75 0 00-1.37.364l-.492 6.861a.75.75 0 001.204.65l1.043-.799.985 3.678a.75.75 0 001.45-.388l-.978-3.646 1.292.204a.75.75 0 00.74-1.16l-3.874-5.764z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					Selecionar
+				</button>
+			</li>
+			<li>
+				<button on:click={() => focusing !== undefined && handleStartUpdateStory(focusing)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="w-5 h-5"
+					>
+						<path
+							d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z"
+						/>
+						<path
+							d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z"
+						/>
+					</svg>
+					Editar
+				</button>
+			</li>
+			<li>
+				<button
+					class="text-error"
+					on:click={() => focusing !== undefined && handleStartDeleteStory(focusing)}
 				>
-					<path
-						fill-rule="evenodd"
-						d="M10 1a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 1zM5.05 3.05a.75.75 0 011.06 0l1.062 1.06A.75.75 0 116.11 5.173L5.05 4.11a.75.75 0 010-1.06zm9.9 0a.75.75 0 010 1.06l-1.06 1.062a.75.75 0 01-1.062-1.061l1.061-1.06a.75.75 0 011.06 0zM3 8a.75.75 0 01.75-.75h1.5a.75.75 0 010 1.5h-1.5A.75.75 0 013 8zm11 0a.75.75 0 01.75-.75h1.5a.75.75 0 010 1.5h-1.5A.75.75 0 0114 8zm-6.828 2.828a.75.75 0 010 1.061L6.11 12.95a.75.75 0 01-1.06-1.06l1.06-1.06a.75.75 0 011.06 0zm3.594-3.317a.75.75 0 00-1.37.364l-.492 6.861a.75.75 0 001.204.65l1.043-.799.985 3.678a.75.75 0 001.45-.388l-.978-3.646 1.292.204a.75.75 0 00.74-1.16l-3.874-5.764z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-				Selecionar
-			</button>
-		</li>
-		<li>
-			<button on:click={() => focusing !== undefined && handleStartUpdateStory(focusing)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="w-5 h-5"
-				>
-					<path
-						d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z"
-					/>
-					<path
-						d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z"
-					/>
-				</svg>
-				Editar
-			</button>
-		</li>
-		<li>
-			<button
-				class="text-error"
-				on:click={() => focusing !== undefined && handleStartDeleteStory(focusing)}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="w-5 h-5"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-				Remover
-			</button>
-		</li>
-	</ul>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="w-5 h-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					Remover
+				</button>
+			</li>
+		</ul>
+		<form method="dialog" class="modal-backdrop bg-transparent">
+			<button>close</button>
+		</form>
+	</dialog>
 </div>
