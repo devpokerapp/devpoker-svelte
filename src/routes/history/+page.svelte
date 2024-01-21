dab846eb01f2aa165712c0091768cc046005c5f3
Arthur <rthurmedeiros@gmail.com>
2024-01-21 15:26:11 -0300

    Added message for empty history

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -76,6 +76,24 @@
 					Minhas sessões participadas
 				</div>
 				<p>Observe informações sobre suas sessões anteriores.</p>
+				{#if $participations.length < 1 && $pokers.length < 1}
+					<div class="alert alert-info">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							class="stroke-current shrink-0 w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<div>Você ainda não participou de nenhuma sessão!</div>
+					</div>
+				{/if}
 				{#each $pokers as poker}
 					<article class="card border">
 						<div class="card-body">
