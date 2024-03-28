48d86bcab1af96252e28f7ae8f7e2ad1cf7925f2
Arthur <rthurmedeiros@gmail.com>
2024-03-28 01:11:12 -0300

    Added error pages

---
+++ src/routes/error/forbidden/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	import AuthButton from '../../../components/AuthButton.svelte';
+</script>
+
+<svelte:head>
+	<title>Erro - Acesso não permitido</title>
+</svelte:head>
+
+<section class="bg-base-200">
+	<div class="absolute top-2 right-2">
+		<AuthButton />
+	</div>
+	<div class="hero min-h-screen">
+		<div class="hero-content text-left">
+			<div class="max-w-lg">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M11.484 2.17a.75.75 0 0 1 1.032 0 11.209 11.209 0 0 0 7.877 3.08.75.75 0 0 1 .722.515 12.74 12.74 0 0 1 .635 3.985c0 5.942-4.064 10.933-9.563 12.348a.749.749 0 0 1-.374 0C6.314 20.683 2.25 15.692 2.25 9.75c0-1.39.223-2.73.635-3.985a.75.75 0 0 1 .722-.516l.143.001c2.996 0 5.718-1.17 7.734-3.08ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75ZM12 15a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75v-.008a.75.75 0 0 0-.75-.75H12Z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+
+				<h1 class="text-5xl font-bold">Acesso não permitido!</h1>
+				<p class="py-6">
+					Não foi possível acessar o recurso. Por favor, verifique suas permissões e tente
+					novamente.
+				</p>
+				<a class="btn btn-primary" href="/">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 16 16"
+						fill="currentColor"
+						class="w-4 h-4"
+					>
+						<path
+							d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z"
+						/>
+					</svg>
+					Voltar para o início
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ src/routes/error/notfound/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	import AuthButton from '../../../components/AuthButton.svelte';
+</script>
+
+<svelte:head>
+	<title>Erro - Não encontrado</title>
+</svelte:head>
+
+<section class="bg-base-200">
+	<div class="absolute top-2 right-2">
+		<AuthButton />
+	</div>
+	<div class="hero min-h-screen">
+		<div class="hero-content text-left">
+			<div class="max-w-lg">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+
+				<h1 class="text-5xl font-bold">Não encontrado!</h1>
+				<p class="py-6">
+					Não foi possível encontrar o recurso. Por favor, verifique o método de acesso e tente
+					novamente.
+				</p>
+				<a class="btn btn-primary" href="/">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 16 16"
+						fill="currentColor"
+						class="w-4 h-4"
+					>
+						<path
+							d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z"
+						/>
+					</svg>
+					Voltar para o início
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
