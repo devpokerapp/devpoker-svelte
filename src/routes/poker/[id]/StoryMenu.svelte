34bba428cccf191b33c9d64422cc99fd0a4a042f
Arthur <rthurmedeiros@gmail.com>
2024-03-31 23:19:54 -0300

    Added story moving buttons

--- src/routes/poker/[id]/StoryMenu.svelte
+++ src/routes/poker/[id]/StoryMenu.svelte
@@ -21,6 +21,7 @@
 	let updating: Story | undefined = undefined;
 	let deleting: Story | undefined = undefined;
 	let focusing: Story | undefined = undefined;
+	let focusingIdx: number = -1;
 	let showFocused = false;
 	let focusMenuHeight = 0;
 	let scrollingContainer: HTMLDivElement;
@@ -34,7 +35,6 @@
 		const encoded = encodeURIComponent(value);
 		const datauri = `data:text/csv;charset=UTF-8,${encoded}`;
 		storiesCSVDatauri = datauri;
-		console.log(datauri);
 	};
 
 	const handleCreateStory = async (event: SubmitEvent) => {
@@ -47,6 +47,7 @@
 			const story = await storyContext.create({
 				name: name,
 				description: description,
+				order: 0,
 				pokerId,
 				id: '',
 				createdAt: '',
@@ -153,6 +154,14 @@
 	const handleExport = async () => {
 		// exportAsCSV();
 	};
+
+	const handleMoveUpStory = (entity: Story, index: number) => {
+		// TODO
+	};
+
+	const handleMoveDownStory = (entity: Story, index: number) => {
+		// TODO
+	};
 </script>
 
 <div id="story-menu" class="card-body p-0">
@@ -249,6 +258,7 @@
 						on:click={(ev) => {
 							focusMenuHeight = ev.currentTarget.offsetTop - scrollingContainer.scrollTop;
 							focusing = story;
+							focusingIdx = index;
 							openModal('modal-story-options');
 						}}
 					>
@@ -421,6 +431,42 @@
 						/>
 					</svg>
 					Remover
+				</button>
+			</li>
+			<li class:disabled={focusingIdx === 0}>
+				<button on:click={() => focusing !== undefined && handleMoveUpStory(focusing, focusingIdx)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="w-5 h-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					Subir
+				</button>
+			</li>
+			<li class:disabled={focusingIdx === $stories.length - 1}>
+				<button
+					on:click={() => focusing !== undefined && handleMoveDownStory(focusing, focusingIdx)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="w-5 h-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					Descer
 				</button>
 			</li>
 		</ul>
