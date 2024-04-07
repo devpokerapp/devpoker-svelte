008f97e5c53d52922eacbf95ca4acd7e66a8581b
Arthur <rthurmedeiros@gmail.com>
2024-04-07 17:36:29 -0300

    Changed banner dismiss button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -136,17 +136,8 @@
 				formulário de avaliação.
 			</a>
 		</div>
-		<button class="btn btn-ghost btn-circle btn-sm" on:click={() => (showBanner = false)}>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 16 16"
-				fill="currentColor"
-				class="w-4 h-4"
-			>
-				<path
-					d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"
-				/>
-			</svg>
+		<button class="btn btn-ghost btn-sm" on:click={() => (showBanner = false)}>
+			<span class=""> OK </span>
 		</button>
 	</div>
 </div>
