4d983825ee9910cd1e545e693e64addbbd35a16c
Arthur <rthurmedeiros@gmail.com>
2024-04-07 17:29:10 -0300

    Added system evaluation banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import AuthButton from '../components/AuthButton.svelte';
+
+	let showBanner = true;
 </script>
 
 <svelte:head>
@@ -94,7 +96,7 @@
 		</div>
 	</div>
 </section>
-<footer class="footer p-12 bg-neutral-800 text-neutral-400">
+<footer class="footer p-12 pb-32 bg-neutral-800 text-neutral-400">
 	<aside>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -115,3 +117,36 @@
 		</p>
 	</aside>
 </footer>
+<div id="info-banner" class="fixed bottom-0 w-full" class:hidden={!showBanner}>
+	<div class="alert alert-info rounded-none">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+			<path
+				fill-rule="evenodd"
+				d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+				clip-rule="evenodd"
+			/>
+		</svg>
+		<div>
+			Este projeto é parte do meu TCC. Para contribuir você pode preencher o
+			<a
+				class="link"
+				href="https://docs.google.com/forms/d/e/1FAIpQLSdCKdzOE516gTyfLtNGH2UgE9u4QiSZehb38mSwAgJMuQSZnw/viewform"
+				target="_blank"
+			>
+				formulário de avaliação.
+			</a>
+		</div>
+		<button class="btn btn-ghost btn-circle btn-sm" on:click={() => (showBanner = false)}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 16 16"
+				fill="currentColor"
+				class="w-4 h-4"
+			>
+				<path
+					d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"
+				/>
+			</svg>
+		</button>
+	</div>
+</div>
